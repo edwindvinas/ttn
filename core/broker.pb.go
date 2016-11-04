7fce6394aa86cbe6f0efea016b4df8d67fe65697
Edwin Vinas <edwin.vinas@opti-knights.com>
2016-11-04 09:28:20 +0000

    grpc.SupportPackageIsVersion4

--- core/broker.pb.go
+++ core/broker.pb.go
@@ -126,7 +126,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion
+const _ = grpc.SupportPackageIsVersion4
 
 // Client API for Broker service
 
--- core/collector.pb.go
+++ core/collector.pb.go
@@ -116,7 +116,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion
+const _ = grpc.SupportPackageIsVersion4
 
 // Client API for CollectorManager service
 
